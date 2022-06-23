d31536dc84d96de4e0a7dbdc16c168c8d1cf5091
sk281 <antoniocabralbraga1993@gmail.com>
2022-06-23 21:47:19 +0100

    final part

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+//console.log("Hello World");
 
 
 function computerPlay(){
@@ -9,9 +9,14 @@ function computerPlay(){
 
 
 
-let choise = prompt("rock, paper, sissors");
+//let choise = prompt("rock, paper, sissors");
 
-function Fight_(playerchoice, computerchoice){
+function Fight_(playerchoice,computerchoice){
+   // let playerchoice = prompt("rock, paper, sissors");
+    if(playerchoice == null){
+    return 'banana';
+    }   
+    playerchoice = playerchoice.toLowerCase();
     console.log('player choice: ' + playerchoice)
     console.log('computer choice: ' + computerchoice);
     if(playerchoice != "rock" && playerchoice != 'paper' && playerchoice != 'sissors'){
@@ -56,4 +61,65 @@ function Fight_(playerchoice, computerchoice){
 }
 
 
-console.log(Fight_(choise,computerPlay()));
+//console.log(Fight_(computerPlay()));
+
+function game(){
+
+    roundPlayer = 0;
+    roundBot = 0;
+
+
+    for (let i = 0; i < 999; i++) {
+            let playerchoice = prompt('rock, paper , sissors');
+            let asd = Fight_(playerchoice,computerPlay());
+            //console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
+            
+            if(asd == 'you win!'){
+                roundPlayer += 1;
+            
+                if(roundPlayer >= 5 ){
+                console.log('You won the  match!')
+                console.log('You:' + roundPlayer + '  ||Bot:' + roundBot  );
+                return null;
+                    } 
+                else if(roundBot >=5){
+                    console.log('You lost the  match!')
+                    console.log('You:' + roundPlayer + '  ||Bot:' + roundBot  );
+                    return null;
+                    }
+                }
+            else if(asd == 'computer wins!'){
+                roundBot += 1;
+                if(roundPlayer >= 5 ){
+
+                    console.log('You won the  match!')
+                    console.log('You:' + roundPlayer + '  ||Bot:' + roundBot  );
+                    return null;
+                        } 
+                    else if(roundBot >=5){
+
+                        console.log('You lost the  match!')
+                        console.log('You:' + roundPlayer + '  ||Bot:' + roundBot  );
+                        return null;
+                        }
+            }
+            else if(asd == 'error typing'){
+
+                console.log('error typing');
+                console.log('You:' + roundPlayer + '  ||Bot:' + roundBot  );
+                continue;
+            }
+
+            else if(asd == 'banana'){
+               return null;
+            }
+          
+            
+            console.log('You:' + roundPlayer + '  ||Bot:' + roundBot  );
+      }
+
+
+}
+
+
+game();
