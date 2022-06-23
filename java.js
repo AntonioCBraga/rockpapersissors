ef78d6f00f56d899b1d3b2f0572e8a3e948782ab
sk281 <antoniocabralbraga1993@gmail.com>
2022-06-23 21:01:25 +0100

    p2

--- java.js
+++ java.js
@@ -7,4 +7,53 @@ function computerPlay(){
     return choice[  Math.floor(Math.random()*3)];
 }
 
-console.log(computerPlay())
+
+
+let choise = prompt("rock, paper, sissors");
+
+function Fight_(playerchoice, computerchoice){
+    console.log('player choice: ' + playerchoice)
+    console.log('computer choice: ' + computerchoice);
+    if(playerchoice != "rock" && playerchoice != 'paper' && playerchoice != 'sissors'){
+        return 'error typing';
+    }
+    else{
+       if (playerchoice == 'rock'){
+            if(computerchoice == 'rock'){
+                return 'draw';
+            }
+            else if(computerchoice =='paper'){
+                return 'computer wins!';
+            }
+            else {
+                return 'you win!';
+            }
+       }
+       else if (playerchoice == 'paper'){
+            if(computerchoice == 'rock'){
+                return 'you win!';
+            }
+            else if(computerchoice =='paper'){
+                return 'draw';
+            }
+            else {
+                return 'computer wins!';
+            }
+        }
+        else{
+            if(computerchoice == 'rock'){
+                return 'computer win!';
+            }
+            else if(computerchoice =='paper'){
+                return 'you win!';
+            }
+            else {
+                return 'draw';
+            } 
+        }
+
+    }
+}
+
+
+console.log(Fight_(choise,computerPlay()));
