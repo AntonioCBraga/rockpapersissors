2f9d529b640149b38e16ec6a59abf1644e33574c
sk281 <antoniocabralbraga1993@gmail.com>
2022-07-05 16:57:54 +0100

    just needscosmetics

--- java.js
+++ java.js
@@ -1,5 +1,66 @@
 //console.log("Hello World");
 
+let playerScore = 0;
+let computerScore = 0;
+var scorep = document.getElementById('scoreplayer');
+var scoreb = document.getElementById('scorebot');
+var spli  = document.getElementById("spli");
+
+let butn = document.createElement("button");
+butn.innerHTML= "Play Again";
+butn.style.marginLeft= "880px";
+butn.style.position= "absolute";
+butn.style.bottom = "440px";
+
+
+const papir = document.getElementsByClassName("btne")[0];
+const sissor = document.getElementsByClassName("btne")[1];
+const rocki = document.getElementsByClassName("btne")[2];
+
+const papiri = document.getElementsByClassName('btn')[0];
+const sisori = document.getElementsByClassName('btn')[1];
+const rokki  = document.getElementsByClassName('btn')[2];
+
+const happeh = document.getElemen
+
+const happy = document.getElementsByClassName('myAudio')[0];
+const sad = document.getElementsByClassName('myAudio')[1];
+
+
+function animatron2000(){
+    
+    //bot animation remove
+    rocki.classList.remove('win');
+    papir.classList.remove('win')
+    sissor.classList.remove('win');
+    rocki.classList.remove('lose');
+    papir.classList.remove('lose')
+    sissor.classList.remove('lose');
+    rocki.classList.remove('draw');
+    papir.classList.remove('draw')
+    sissor.classList.remove('draw');
+
+
+
+
+
+    //human anims remove
+    papiri.classList.remove('lose');
+    sisori.classList.remove('lose');
+    rokki.classList.remove('lose');
+    papiri.classList.remove('win');
+    sisori.classList.remove('win');
+    rokki.classList.remove('win');
+    papiri.classList.remove('draw');
+    sisori.classList.remove('draw');
+    rokki.classList.remove('draw');
+
+}
+
+
+
+
+
 
 function computerPlay(){
     var choice = ['rock','paper','sissors'];
@@ -9,117 +70,201 @@ function computerPlay(){
 
 
 
-//let choise = prompt("rock, paper, sissors");
 
 function Fight_(playerchoice,computerchoice){
-   // let playerchoice = prompt("rock, paper, sissors");
-    if(playerchoice == null){
-    return 'banana';
-    }   
-    playerchoice = playerchoice.toLowerCase();
-    console.log('player choice: ' + playerchoice)
-    console.log('computer choice: ' + computerchoice);
+      
+   
     if(playerchoice != "rock" && playerchoice != 'paper' && playerchoice != 'sissors'){
         return 'error typing';
     }
     else{
        if (playerchoice == 'rock'){
             if(computerchoice == 'rock'){
-                return 'draw';
+                animatron2000();
+                rocki.classList.add('draw');
+                rokki.classList.add('draw');
+                
             }
             else if(computerchoice =='paper'){
-                return 'computer wins!';
+                //computer
+                animatron2000();
+                
+                papir.classList.add('win')
+                rokki.classList.add('lose');
+                computerScore += 1;
+                //man
+
+                
             }
             else {
-                return 'you win!';
+                animatron2000();
+                rokki.classList.add('win');
+                sissor.classList.add('lose');
+                
+                playerScore += 1;
+                
+
+                
             }
        }
        else if (playerchoice == 'paper'){
             if(computerchoice == 'rock'){
-                return 'you win!';
+                animatron2000();
+                papiri.classList.add('win');
+                rocki.classList.add('lose');
+                playerScore += 1;
+                //man
             }
             else if(computerchoice =='paper'){
-                return 'draw';
+                animatron2000();
+                papir.classList.add('draw');
+                papiri.classList.add('draw');
+                
+                
             }
             else {
-                return 'computer wins!';
+                animatron2000();
+                sissor.classList.add('win');
+                papiri.classList.add('lose');
+               
+                computerScore += 1;
+                //man
             }
         }
         else{
             if(computerchoice == 'rock'){
-                return 'computer win!';
+                
+                animatron2000();
+                rocki.classList.add('win');
+                sisori.classList.add('lose');
+                
+                computerScore += 1;
+                //man
+
+                
+                
+               
+
+               
+                
+                
             }
             else if(computerchoice =='paper'){
-                return 'you win!';
+                animatron2000();
+
+                sisori.classList.add('win');
+                papir.classList.add('lose');
+                playerScore += 1;
+                
+
             }
             else {
-                return 'draw';
+                
+                animatron2000();
+                sisori.classList.add('draw');
+                sissor.classList.add('draw');
+
             } 
+
+
         }
 
     }
+    
+}
+
+function winner(){
+
+    animatron2000();
+    scoreb.textContent = null;
+    scorep.textContent = null;
+    spli.textContent = null;
+    
+    if(playerScore === 5){
+        playerScore = -9999;
+        computerScore = -9999;
+        happy.play();
+        spli.textContent = "You Win!";
+        playag();
+       
+
+    }
+    else if(computerScore === 5){
+        playerScore = -9999;
+        computerScore = -9999;
+        sad.play();
+        spli.textContent = "You Lose!";
+        playag();
+    
+    }
+    
+}
+
+function playag(){
+    butn.style.visibility = 'visible';
+    document.body.appendChild(butn);
+    
+    
 }
 
 
-//console.log(Fight_(computerPlay()));
-
-function game(){
-
-    roundPlayer = 0;
-    roundBot = 0;
+function myfunc(){
+    console.log('hello');
+}
 
 
-    for (let i = 0; i < 999; i++) {
-            let playerchoice = prompt('rock, paper , sissors');
-            let asd = Fight_(playerchoice,computerPlay());
-            //console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-            
-            if(asd == 'you win!'){
-                roundPlayer += 1;
-            
-                if(roundPlayer >= 5 ){
-                console.log('You won the  match!')
-                console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-                return null;
-                    } 
-                else if(roundBot >=5){
-                    console.log('You lost the  match!')
-                    console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-                    return null;
-                    }
-                }
-            else if(asd == 'computer wins!'){
-                roundBot += 1;
-                if(roundPlayer >= 5 ){
-
-                    console.log('You won the  match!')
-                    console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-                    return null;
-                        } 
-                    else if(roundBot >=5){
-
-                        console.log('You lost the  match!')
-                        console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-                        return null;
-                        }
-            }
-            else if(asd == 'error typing'){
-
-                console.log('error typing');
-                console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-                continue;
-            }
-
-            else if(asd == 'banana'){
-               return null;
-            }
-          
-            
-            console.log('You:' + roundPlayer + '  ' +'  ||Bot:' + roundBot  );
-      }
-
+butn.onclick = function(){
+    spli.textContent = "-";
+    playerScore= 0;
+    computerScore= 0;
+    butn.style.visibility= "hidden";
 
 }
 
 
-game();
+
+
+//window.addEventListener('click',choise );
+
+function clickHandler(e) {
+ 
+    role = this.id;
+    
+    
+    if(role == '1' ){
+        
+        Fight_('paper',computerPlay()) ;
+    }
+    else if(role == '2' ){
+        
+        Fight_('sissors',computerPlay()) ;
+    }
+    else if(role == '3' ){
+        
+        Fight_('rock',computerPlay()) ;
+       
+    }
+    if(playerScore >= 0 &&computerScore >=0 ){
+        scoreb.textContent = computerScore.toString();
+        scorep.textContent = playerScore.toString();
+    }
+    if(playerScore === 5 || computerScore === 5 ){
+        winner();
+    }
+    
+
+
+}   
+
+
+
+
+
+
+
+const btn = document.querySelectorAll('.btn');
+
+btn.forEach(btn => btn.addEventListener('click',(clickHandler)));
+
+
+
